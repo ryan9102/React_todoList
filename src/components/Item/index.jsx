26c5c24e2cbdb32cc90bacf92ac1aa3a2c9c74fc
Hualiang <ryan_li@outlook.com.au>
2021-11-09 07:24:42 +1100

    static webpage

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import './index.css'
 
 export default class Item extends Component {
     render() {
         return (
-            <div>
-                Item...
-            </div>
+            <li>
+            <label>
+              <input type="checkbox"/>
+              <span>xxxxx</span>
+            </label>
+            <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+          </li>
         )
     }
 }
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import Item from "../Item"
+import './index.css'
 
 export default class List extends Component {
     render() {
         return (
-            <div>
-                List...
-            </div>
+            <ul className="todo-main">
+            <Item/>
+            <Item/>
+            <Item/>
+          </ul>
         )
     }
 }
